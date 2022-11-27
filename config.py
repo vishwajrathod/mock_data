54c9a16538376a6a29445394093a454267aadfa1
Vishwaj <vishwajrathod@gmail.com>
2022-11-27 11:16:05 +0530

    modified config

--- config.py
+++ config.py
@@ -3,3 +3,8 @@ import os
 MODEL_FILE_PATH = os.path.join('iris_dataset','logistic_model.pkl')
 JSON_FILE_PATH = os.path.join('iris_dataset','data.json')
 PORT_NUMBER = 5001
+
+var = 'first_name'
+
+if __name__ == "__main__":
+    print('success')
